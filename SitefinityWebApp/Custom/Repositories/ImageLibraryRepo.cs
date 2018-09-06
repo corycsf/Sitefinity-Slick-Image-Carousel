06d45aa3b16d14e6104699712d3c17c1a3dc3a3b
ccarlson <ccarlson@smoothfusion.com>
2018-09-06 11:37:40 -0500

    Init

---
+++ SitefinityWebApp/Custom/Repositories/ImageLibraryRepo.cs
@@ -0,0 +1,40 @@
+﻿using SitefinityWebApp.Mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Telerik.Sitefinity;
+using Telerik.Sitefinity.Model;
+using Telerik.Sitefinity.Modules.Libraries;
+using Telerik.Sitefinity.Pages.Model;
+using Telerik.Sitefinity.RelatedData;
+using Telerik.Sitefinity.Libraries.Model;
+
+namespace SitefinityWebApp.Data
+{
+    public static class ImageLibraryRepo
+    {
+        public static List<HomepageCarouselModel> GetHomepageCarouselItems(Guid imageGalleryID)
+        {
+
+            var rawItems = App.WorkWith().Images()
+                .Where(i => i.Album.Id == imageGalleryID).Published()
+                .OrderBy(i => i.Ordinal).Get().ToList();
+
+            Func<Image, string, string> _getStringValue = (image, val) => image.GetValue(val)?.ToString() ?? string.Empty;
+
+            List<HomepageCarouselModel> retval = rawItems.Select(item => new HomepageCarouselModel
+            {
+                ItemSummary = _getStringValue(item, "ItemSummary"),
+                ItemTitle = _getStringValue(item, "ItemTitle"),
+                ImageUrl = item.Url,
+                PageLink = _getStringValue(item, "PageLink"),
+                AltText = _getStringValue(item, "AlternativeText")
+
+            }).ToList();
+
+            return retval;
+
+        }
+    }
+}
---
+++ SitefinityWebApp/MVC/Controllers/HomepageCarouselController.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using SitefinityWebApp.Data;
+using SitefinityWebApp.Mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Telerik.Sitefinity.Mvc;
+
+namespace SitefinityWebApp.Mvc.Controllers
+{
+    [ControllerToolboxItem(Name = "HomepageCarousel", Title = "Homepage Carousel", SectionName = "MVC")]
+    public class HomepageCarouselController : Controller
+    {
+        // GET: HomepageCarousel
+        public string ImageGalleryId { get; set; }
+        private Guid _imageGalleryId;
+        public ActionResult Index()
+        {
+            List<HomepageCarouselModel> model = GetModel();
+            return View(model);
+        }
+        private List<HomepageCarouselModel> GetModel()
+        {
+            List<HomepageCarouselModel> model = new List<HomepageCarouselModel>();
+            if (!ImageGalleryId.IsNullOrEmpty())
+            {
+                _imageGalleryId = new Guid(ImageGalleryId);
+                model = ImageLibraryRepo.GetHomepageCarouselItems(_imageGalleryId);
+            }
+            return model;
+        }
+    }
+}
